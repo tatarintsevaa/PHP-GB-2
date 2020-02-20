cfb3251b0f232a4a89dd283f44234570c9b88185
tatarintsevaa <tatalexey@gmail.com>
2020-02-20 16:21:37 +0300

    Переписал загрузчик через пространство имен

--- engine/Autoload.php
+++ engine/Autoload.php
@@ -4,22 +4,12 @@
 
 class Autoload
 {
-    //TODO Переписать автозагрузчик через пространство имен
-    private $path = [
-        'models',
-        'interfaces',
-        'engine'
-    ];
-
-    public function loadClass($className) {
-        //TODO убрать цикл и построить из $className правильный путь к классу (.php)
-        foreach ($this->path as $path) {
-            $fileName = "../{$path}/{$className}.php";
-            //var_dump($className);
-            if (file_exists($fileName)) {
-                include $fileName;
-                break;
-            }
-          }
+    public function loadClass($className)
+    {
+        $path = str_replace("\\", "/" , $className);
+        $filename = "../{$path}.php";
+        if (file_exists($filename)) {
+            include $filename;
+        }
     }
 }
